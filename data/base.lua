56ac22f2233374ca7ecf944d78358fb44eff7c65
yipf <yipf@debian>
2015-10-15 13:59:15 +0800

    beta

--- data/base.lua
+++
@@ -1,8 +0,0 @@
-return {
-
---~ 	{drawer="box 2",texture="color 255 255 255"}, 		--  BOX
-	{matrix={10,0,0,0,   0,1,0,0,   0,0,1,0,   2.2,0.2,0,1},drawer="box 0.2",texture="color 255 0 0"}, 		--  X axis
-	{matrix={1,0,0,0,   0,10,0,0,   0,0,1,0,   0,2,0,1},drawer="box 0.2",texture="color 0 255 0"},			--  Y axis
-	{matrix={1,0,0,0,   0,1,0,0,   0,0,10,0,   0,0.2,2.2,1},drawer="box 0.2",texture="color 0 0 255"},  		--  Z axis
-	{drawer="plain 150 -0.1",	texture="chess 16 16 <255 255 255> <192 192 192>",},
-}
